b1cbd145ca63d67a2921126e80934d02a2797fc7
MarcinBalejko <mbbm9@o2.pl>
2019-12-23 14:29:11 +0100

    :recycle: Todos get added

--- src/App.js
+++ src/App.js
@@ -46,7 +46,12 @@ class App extends Component {
 
   // Add Todo
   addTodo = (title) => {
-    console.log(title)
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
   }
 
   render() {
