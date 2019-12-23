5b870aef7cb8f8ce603515c9fd3d445241507e2e
MarcinBalejko <mbbm9@o2.pl>
2019-12-23 17:14:00 +0100

    :white_check_mark: Axios get request

--- src/App.js
+++ src/App.js
@@ -5,30 +5,19 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Dinner with wife',
-        completed: true
-      },
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      }
-    ]
+    todos: []
   }
 
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({ todos: res.data }))
+  }
 
   // Toggle Complete
   markComplete = (id) => {
