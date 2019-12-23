e1ce6c356c20b2f66378b6db9e77ff58302338ac
MarcinBalejko <mbbm9@o2.pl>
2019-12-23 03:07:53 +0100

    :alembic: 'completed' parameter in state changes when the box is checked

--- src/App.js
+++ src/App.js
@@ -24,7 +24,14 @@ class App extends Component {
   }
 
   markComplete = (id) => {
-    console.log(id)
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
   }
 
 
