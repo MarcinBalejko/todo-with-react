afdbc81c430f7328fc2677e999ac1d6561687c61
MarcinBalejko <mbbm9@o2.pl>
2019-12-23 21:34:29 +0100

    :recycle: Imported Reactstrap

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 
 export class TodoItem extends Component {
     getStyle = () => {
